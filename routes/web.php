98f7bf57ee4113d5e25c00107ac54a8663ffe95e
Roberto <it.robramos@gmail.com>
2022-02-25 03:54:12 -0600

    data method get en entradas de mercancía

--- routes/web.php
+++ routes/web.php
@@ -54,7 +54,7 @@ Route::get('/entries/add','EntryController@create')->name('entries.add');
 Route::post('/entries/store','EntryController@store')->name('entries.store');
 Route::get('/entries/edit/{id}','EntryController@edit')->name('entries.edit');
 Route::post('/entries/edit/{id}','EntryController@update')->name('entries.update');
-Route::delete('/entries/destroy/{id}','EntryController@destroy')->name('entries.destroy');
+Route::get('/entries/destroy/{id}','EntryController@destroy')->name('entries.destroy');
 Route::get('/entries/show/{id}','EntryController@show')->name('entries.show');
 
 Route::get('entries/products','EntryController@products')->name('entries.products');
