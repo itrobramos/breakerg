e3b6ce826ed593e33dde215d961c8595de91c244
Roberto <it.robramos@gmail.com>
2022-03-02 00:17:41 -0600

    Reactivación de usuario eliminado

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,12 +15,12 @@ class UserController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     public function index(Request $request)
     {
         $query = User::orderBy('name', 'asc');
         $users = $query->paginate();
-        return view('users.index',compact('users'));
+        return view('users.index', compact('users'));
     }
 
     public function create()
@@ -35,58 +35,64 @@ class UserController extends Controller
             'email' => 'required',
         ]);
 
-        $existingUser = User::where('email', $request->email)->where('deleted_at', null)->first();
+        $existingUser = User::where('email', $request->email)->first();
 
-        if(isset($existingUser)){
-            return redirect('users')->with('danger','El usuario ingresado ya existe.');
+        if (isset($existingUser)) {
+            if ($existingUser->deleted_at != null) {
+                //Se reactiva
+                $existingUser->deleted_at = null;
+                $existingUser->save();
+            } else {
+                //Avisar que ya existe
+                return redirect('users')->with('danger', 'El usuario ingresado ya existe.');
+            }
+        } else {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charactersLength = strlen($characters);
+            $randomString = '';
+            for ($i = 0; $i < 8; $i++) {
+                $randomString .= $characters[rand(0, $charactersLength - 1)];
+            }
+
+            $User = new User();
+            $User->name = $request->name;
+            $User->email = $request->email;
+            $User->password = Hash::make($randomString);
+
+            if (isset($request->admin))
+                $User->admin = true;
+            else
+                $User->admin = false;
+
+            $User->save();
+
+            $destinatario = $User->email;
+            $msg = "Ha sido invitad@ a colaborar en el sistema de Breaker G. \n\n "  .
+
+                "Para ingresar al sistema ingrese a: " . env('APP_URL') . "\n" .
+                "Su usario es: " . $User->email . " \n" .
+                "Contraseña temporal: " . $randomString;
+
+            $data = [];
+
+            try {
+                Mail::send(['email' => 'xxx'], $data, function ($message) use ($destinatario, $msg) {
+                    $message->from('no-reply@breakerg.com', 'BreakerG');
+                    $message->to($destinatario);
+                    $message->subject('Bienvenido a Breaker G');
+                    $message->setBody($msg);
+                });
+            } catch (\Throwable $th) {
+            }
+
+            return redirect('users')->with('success', 'Usuario creado correctamente. Revise la bandera de entrada del usuario.');
         }
-
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < 8; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-
-        $User = new User();
-        $User->name = $request->name;
-        $User->email = $request->email;
-        $User->password = Hash::make($randomString);
-
-        if(isset($request->admin))
-            $User->admin = true;
-        else
-            $User->admin = false;
-
-        $User->save();
-        
-        $destinatario = $User->email;
-        $msg = "Ha sido invitad@ a colaborar en el sistema de Breaker G. \n\n "  . 
-                
-        "Para ingresar al sistema ingrese a: " . env('APP_URL') . "\n" .
-        "Su usario es: " . $User->email . " \n" . 
-        "Contraseña temporal: " . $randomString;
-
-        $data = [];
-
-        try{
-            Mail::send(['email'=>'xxx'],$data, function ($message) use ($destinatario, $msg) {
-                $message->from('no-reply@breakerg.com', 'BreakerG');
-                $message->to($destinatario);
-                $message->subject('Bienvenido a Breaker G');
-                $message->setBody($msg);
-            });
-            
-        } catch (\Throwable $th) {
-        }
-
-        return redirect('users')->with('success','Usuario creado correctamente. Revise la bandera de entrada del usuario.');
     }
 
     public function edit($id)
     {
         $user = User::findOrFail($id);
-        return view('users.edit',compact('user'));
+        return view('users.edit', compact('user'));
     }
 
     public function update(Request $request, $id)
@@ -100,37 +106,35 @@ class UserController extends Controller
         $User->name = $request->name;
         $User->email = $request->email;
 
-        if(isset($request->admin))
+        if (isset($request->admin))
             $User->admin = true;
         else
             $User->admin = false;
 
         $User->save();
 
-        return redirect('users')->with('success','Usuario editado correctamente.');
+        return redirect('users')->with('success', 'Usuario editado correctamente.');
     }
 
     public function destroy($id)
     {
         User::destroy($id);
-        return redirect('users')->with('success','Usuario eliminado correctamente.');
+        return redirect('users')->with('success', 'Usuario eliminado correctamente.');
     }
 
 
-    public function changePassword(){
+    public function changePassword()
+    {
         $user = User::findOrFail(Auth::user()->id);
-        return view('users.changePassword',compact('user'));
+        return view('users.changePassword', compact('user'));
     }
 
-    public function updatePassword(Request $request ){
+    public function updatePassword(Request $request)
+    {
         $user = User::find(Auth::user()->id);
         $user->password = Hash::make($request->password);
         $user->save();
 
-        return redirect('users/changePassword')->with('success','Contraseña cambiada correctamente.');
+        return redirect('users/changePassword')->with('success', 'Contraseña cambiada correctamente.');
     }
-
-   
-
-   
 }
